7475de00c8d36286dcb959cbed3519fcfd51f0ea
saif khan <saifbusiness29@gmail.com>
2023-10-10 10:11:52 +0530

    header and footer added

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Online Store</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
